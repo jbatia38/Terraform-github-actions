77a34e6de9a89633f39b0e26a5a6a0fbce02d154
John Batia <wwwwww@wwww.www>
2023-11-19 08:53:21 -0600

    added plan

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "terraform-git-backend"
     key    = "path/s3-bucket"
     region = "us-east-1"
-    dynamodb_table = "terraform-table"
+    dynamodb_table = "lock-table"
   }
 }
